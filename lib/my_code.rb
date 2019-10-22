2c2b1431d37378393f312240dc7bba1fcb3e8656
Pavel Beletsky <pavel.beletsky@live.com>
2019-10-22 03:21:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -51,15 +51,29 @@ def reduce_to_total(source_array, starting_point = 0)
   total
 end
 
+#def reduce_to_all_true(source_array)
+#  index = 0
+#  while index < source_array.count do
+#    if !source_array[index]
+#      return FALSE
+#    end
+#    index += 1 
+#  end
+#  return TRUE
+#end
+
 def reduce_to_all_true(source_array)
+  result = TRUE
   index = 0
   while index < source_array.count do
-    if !source_array[index]
-      return FALSE
+    if source_array[index]
+      index += 1
+    else
+      result = FALSE
+      index += 1
     end
-    index += 1 
   end
-  return TRUE
+  result
 end
 
 def reduce_to_any_true(source_array)
